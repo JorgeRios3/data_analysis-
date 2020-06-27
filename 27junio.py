3895e2b6085f78d4abe6d784f4f9bfa9caf8c0ef
Jorge Rios <jorge.rios@admins-MacBook-Pro-8.local>
2020-06-27 14:51:15 -0500

    dia 27 de junio

--- 27junio.py
+++ 27junio.py
@@ -33,10 +33,19 @@ import numpy as np
 
 
 
-
+#leer archivo csv
 #with open('personas.txt', 'r') as f:
 #    personas = list(csv.reader(f, delimiter=','))
 #    print(personas)
+
+#moficar archivo csv
+#with open("personas.txt", "w") as f:
+#   for x in personas:
+#     linea = "{}\n".format(",".join(x))
+#
+#      f.write(linea)
+
+
 
 
 #personas = '[{"nombre":"jorge", "apellido":"rios", "edad":29}, {"nombre":"jair", "apellido":"beltran", "edad":22}]'
@@ -48,3 +57,45 @@ import numpy as np
 
 
 
+
+#a=[None,None,None,None]
+
+#numpy functiones
+#y = np.random.random((3,4))
+#y = np.zeros((3,4))
+#y = np.ones((3,4))
+#np.shape(y)
+#y = np.random.random((5,1,4))
+#+, -, *, / or %
+#np.add(), np.subtract(), np.multiply(), np.divide() and np.remainder()
+#b.median()
+
+
+#arrays indexes
+#lista=[1,2,3,4,5]
+#ultimo valor lista[-1]
+#ultimos 2 lista[-2:]
+#primeros 2 lista[:2]
+#valores de pocisiones lista[0:4]
+
+#lista2= [[1,2,3], [4,5,6]]
+#ultomo valor lista2[-1][-1]
+
+
+
+#manejo de lista
+#ordernar lista
+#[1,3,3,2].sort()
+
+#manejo de json
+
+#convertir de string a json json.loads('[{"nombre":"apellid"}]')
+#convertir de json a string json.dumps([{"nombre":"apellid"}])
+
+#dicionario_anidado = [{"nombre":"jorge", "direcciones":[{"numero":1244},{"numero":234, "color":"blanco"}]}]
+#dicionario_anidado[0]["direcciones"][1]["color"]
+
+
+#order por tamaño dentro de lista con diccionarios
+#import operator
+#lista.sort(key=operator.itemgetter('size'))
